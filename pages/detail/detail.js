979446fee013a9f2f368b0f836f289f81f155677
mstimes <mstimes_202105@126.com>
2021-10-08 17:38:14 +0800

    add num select

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -18,7 +18,8 @@ Page({
     globalDetail: '',
     globalId: '', 
     globalImageUrl: '',
-    
+    num: 1,
+		minusStatus: 'disabled',
   },
   //轮播图的切换事件
   swiperChange: function(e) {
@@ -219,5 +220,42 @@ Page({
       specificButtons: this.data.specificButtons,
     })
   },
+
+  /* 点击减号 */
+	bindMinus: function() {
+		var num = this.data.num;
+		// 如果大于1时，才可以减
+		if (num > 1) {
+			num --;
+		}
+		// 只有大于一件的时候，才能normal状态，否则disable状态
+		var minusStatus = num <= 1 ? 'disabled' : 'normal';
+		// 将数值与状态写回
+		this.setData({
+			num: num,
+			minusStatus: minusStatus
+		});
+	},
+	/* 点击加号 */
+	bindPlus: function() {
+		var num = this.data.num;
+		// 不作过多考虑自增1
+		num ++;
+		// 只有大于一件的时候，才能normal状态，否则disable状态
+		var minusStatus = num < 1 ? 'disabled' : 'normal';
+		// 将数值与状态写回
+		this.setData({
+			num: num,
+			minusStatus: minusStatus
+		});
+	},
+	/* 输入框事件 */
+	bindManual: function(e) {
+		var num = e.detail.value;
+		// 将数值与状态写回
+		this.setData({
+			num: num
+		});
+	}
 })
 
